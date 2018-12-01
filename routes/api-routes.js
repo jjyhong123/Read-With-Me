bbc35606ccdf24cc2cf75c90fd70a1084b51722f
Jae <jjyhong123@gmail.com>
2018-11-30 22:13:11 -0800

    retry

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -85,9 +85,11 @@ router.post(
   '/add',
   singleUpload,
   (req, res) => {
+    console.log(req.file.location)
     client
     .textDetection(req.file.location)
     .then(results => {
+      console.log(results)
       let detections = results[0].textAnnotations[0];
       if (detections) {
         console.log(detections)
@@ -103,26 +105,6 @@ router.post(
     .catch(err => {
       console.error('ERROR:', err);
     });
-
-    /*
-    client
-    .textDetection(url)
-    .then(results => {
-      let detections = results[0].textAnnotations[0];
-      if (detections) {
-        let language = detections.locale;
-        let text = detections.description.replace(new RegExp('\\n', 'g'), ' ')
-        let image = req.file.location;
-        let speaker = convertLanguageToSpeaker(language);
-        handleTextToVoice(speaker, text, image, req, res);
-      } else {
-        res.render("picture", { user: req.user, err: "No text detected in image." })
-      }
-    })
-    .catch(err => {
-      console.error('ERROR:', err);
-    });
-    */
     
   }
 );
