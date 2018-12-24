fa538e6f86de39437532847658121c6dc2268739
Jae <jjyhong123@gmail.com>
2018-12-24 03:20:20 -0800

    made a fresh node_modules

--- preinstall.js
+++ preinstall.js
@@ -1,2 +1,2 @@
 const fs = require("fs");
-fs.writeFile("./read-with-me-auth-credentials.json", process.env.google_config, (err) => {if (!err) {console.log("Preinstall complete")} else console.log(err)});
+fs.writeFile("./read-with-me-auth-credentials.json", process.env.google_config, (err) => {if (!err) {console.log(process.env.google_config)} else console.log(err)});
